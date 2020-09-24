4d8fcce7c56a82d65502bad45ce412211a222432
pankaj <pankajswami69@gmail.com>
2020-09-24 21:03:43 +0530

    Working with props and it's children

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
     <div className="App">
       <h1>Hi, I'm  a React App</h1>
       <p>This is really working!</p>
-      <Person name="Max" age={25} />
-      <Person name="Stephanie" age={28} />
-      <Person name="Harsh" age={30} />
+      <Person name="Max" age="25" />
+      <Person name="Stephanie" age="28">Drake is on break</Person>
+      <Person name="Harsh" age="30" />
     </div>
   );
 }
