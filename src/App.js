802e849a9bc30f60377a12abd86b8637a269a9e2
pankaj <pankajswami69@gmail.com>
2020-09-24 20:49:22 +0530

    Working with props

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
     <div className="App">
       <h1>Hi, I'm  a React App</h1>
       <p>This is really working!</p>
-      <Person />
-      <Person />
-      <Person />
+      <Person name="Max" age={25} />
+      <Person name="Stephanie" age={28} />
+      <Person name="Harsh" age={30} />
     </div>
   );
 }
