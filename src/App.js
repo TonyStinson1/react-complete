d108d0a7b6b137f3fe4e46087cdf153a37aed335
pankaj <pankajswami69@gmail.com>
2020-09-25 15:14:05 +0530

    Inline styling

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Person from './Person/Person';
 
 class App extends Component {
 
-  state={
+  state = {
     persons: [
       { name: 'Max', age: 28 },
       { name: 'Stephanie', age: 29 },
@@ -16,9 +16,9 @@ class App extends Component {
     // console.log('Was clicked!');
     this.setState({
       persons: [
-      { name: newName, age: 28 },
-      { name: 'Stephanie', age: 29 },
-      { name: 'Harshita', age: 65 },
+        { name: newName, age: 28 },
+        { name: 'Stephanie', age: 29 },
+        { name: 'Harshita', age: 65 },
       ]
     })
   }
@@ -27,19 +27,27 @@ class App extends Component {
     // console.log('Was clicked!');
     this.setState({
       persons: [
-      { name: 'Max', age: 28 },
-      { name: event.target.value, age: 29 },
-      { name: 'Harshita', age: 65 },
+        { name: 'Max', age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: 'Harshita', age: 65 },
       ]
     })
   }
 
+
   render() {
+    const styles = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
     return (
       <div className="App">
         <h1>Hi, I'm  a React App</h1>
         <p>This is really working!</p>
-        <button onClick={() => this.switchNameHandler('Bradley')}>Switch Name</button> {/* inefficient */}
+        <button onClick={() => this.switchNameHandler('Bradley')} style={styles}>Switch Name</button> {/* inefficient */}
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person click={this.switchNameHandler.bind(this, 'Max')} changed={this.nameChangedHandler.bind(this)} name={this.state.persons[1].name} age={this.state.persons[1].age} >I'm a Coder</Person> {/* use this mostly */}
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
