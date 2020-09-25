25599c87027cce86b8dee189cd544b7a6f7c9745
pankaj <pankajswami69@gmail.com>
2020-09-25 12:57:10 +0530

    Working with states and making use of events

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi, I'm  a React App</h1>
-      <p>This is really working!</p>
-      <Person name="Max" age="25" />
-      <Person name="Stephanie" age="28">Drake is on break</Person>
-      <Person name="Harsh" age="30" />
-    </div>
-  );
+class App extends Component {
+
+  state={
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Stephanie', age: 29 },
+      { name: 'Harsh', age: 30 },
+    ]
+  }
+
+  switchNameHandler = () => {
+    // console.log('Was clicked!');
+    this.setState({
+      persons: [
+      { name: 'Bradley', age: 28 },
+      { name: 'Stephanie', age: 29 },
+      { name: 'Harshita', age: 65 },
+      ]
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm  a React App</h1>
+        <p>This is really working!</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >I'm a Coder</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+      </div>
+    );
+  }
 }
 
 export default App;
